86c98c813e19c018ee2d7bf9a8b810dc2c53114e
mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-17 12:33:55 +0300

    bound the github component to the app component and vice versa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { GithubComponent } from './github/github.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Github-app';

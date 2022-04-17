836665d443661fe536fffe5bd9b3a4efffc44610
mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-17 12:18:34 +0300

    add a github component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"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"
 };
 
 /*

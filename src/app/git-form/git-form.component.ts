8acf38cd7cc04bf90e4d2975554fed9937bfd05c
mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-17 22:21:07 +0300

    add imports and exports to the form component

--- src/app/git-form/git-form.component.ts
+++ src/app/git-form/git-form.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { HttpServiceService } from '../http-service.service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-git-form',
   templateUrl: './git-form.component.html',
   styleUrls: ['./git-form.component.css']
 })
 export class GitFormComponent implements OnInit {
+  user!:User;
+  userName!:string;
+  httpsevice!:HttpServiceService
 
-  constructor() { }
+
+  constructor(httpsevice:HttpServiceService) {
+    this.httpsevice =httpsevice;
+   }
 
   ngOnInit(): void {
   }
